92d5fe24c7e50e73da817ca56b1b822892d9c232
SepanTsai <ch03282012@gmail.com>
2021-10-13 02:42:28 +0800

    [NEW]Update Nav Bar

---
+++ resources/js/components/navmenu-setting.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-dropdown style="position: absolute; top: 20px; right: 20px; font-size: 18px;">
+        <span class="el-dropdown-link" style="font-weight: bold;">
+            <span style="position: relative; top: -2px;">Setting &nbsp;</span><i class="el-icon-setting" style="font-size: 125%"></i>
+        </span>
+        <template #dropdown>
+            <el-dropdown-menu>
+                <el-dropdown-item icon="el-icon-plus">Action 1</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-circle-plus">Action 2</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-circle-plus-outline">Action 3</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-check">Action 4</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-circle-check">Action 5</el-dropdown-item>
+            </el-dropdown-menu>
+        </template>
+    </el-dropdown>
+</template>
+<style>
+.el-dropdown-link {
+    cursor: pointer;
+    color: #ffd04b;
+}
+</style>
--- resources/js/components/navmenu.vue
+++ resources/js/components/navmenu.vue
@@ -1,7 +1,7 @@
 <template>
     <el-menu
         :uniqueOpened="true"
-        default-active="1-4-1"
+        default-active="1-1"
         class="el-menu-demo"
         mode="horizontal"
         @open="handleOpen"
@@ -9,6 +9,11 @@
         background-color="#545c64"
         text-color="#fff"
         active-text-color="#ffd04b">
+        <el-menu-item index="0">
+            <a href="http://localhost/home/">
+                <span style="font-weight: bold; font-size: 18px;">Sepan</span>
+            </a>
+        </el-menu-item>
         <el-sub-menu index="1">
             <template #title>
                 <i class="el-icon-location"></i>
@@ -22,13 +27,31 @@
                 <el-menu-item index="1-4-1">Option 1</el-menu-item>
             </el-sub-menu>
         </el-sub-menu>
-        <el-menu-item index="2"><i class="el-icon-menu"></i>List 2</el-menu-item>
-        <el-menu-item index="3"><i class="el-icon-document"></i>List 3</el-menu-item>
-        <el-menu-item index="4"><i class="el-icon-setting"></i>List 4</el-menu-item>
+        <el-sub-menu index="2">
+            <template #title>
+                <i class="el-icon-setting"></i>
+                <span>List 2</span>
+            </template>
+            <el-menu-item index="2-1">Option 1</el-menu-item>
+            <el-menu-item index="2-2">Option 2</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="3">
+            <template #title>
+                <i class="el-icon-menu"></i>
+                <span>List 3</span>
+            </template>
+            <el-menu-item index="3-1">Option 1</el-menu-item>
+            <el-menu-item index="3-2">Option 2</el-menu-item>
+        </el-sub-menu>
+        <navmenu_setting></navmenu_setting>
     </el-menu>
 </template>
 <script>
+import navmenu_setting from './navmenu-setting'
 export default {
+    data() {
+        return {}
+    },
     methods: {
         handleOpen(key, keyPath) {
             console.log(key, keyPath);
@@ -39,6 +62,9 @@ export default {
     },
     mounted() {
         console.log(this.$route);
+    },
+    components: {
+        navmenu_setting
     }
 };
 </script>
